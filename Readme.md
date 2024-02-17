15d7f8d8b531fc48b6ef31ecd8cdabbd0a084961
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2024-02-17 22:04:06 +0530

    Added readme,shell scripts

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# InstantID CPU
+
+InstantID CPU inference with less memory requirement(11GB RAM).
+
+## Prerequisites
+
+- Python 3.10 or higher
+- 11 GB system RAM
+- Active internet connection to install and download models
+
+## How to run
+
+Follow these steps :
+
+### Windows
+
+- Double click install.bat(It will take some time to install,depending on your internet speed.)
+- To start desktop GUI double click start.bat
+
+## Linux
+
+Run the following command in the terminal
+
+- Clone/download this repo
+- In the terminal, enter into instantidcpu directory
+- `chmod +x install.sh`
+- `./install.sh`
+- `./start.sh`
+
+## Disclaimer
+
+The code of InstantID is released under [Apache License](https://github.com/InstantID/InstantID?tab=Apache-2.0-1-ov-file#readme) for both academic and commercial usage. **However, both manual-downloading and auto-downloading face models from insightface are for non-commercial research purposes only** according to their [license](https://github.com/deepinsight/insightface?tab=readme-ov-file#license). Users are granted the freedom to create images using this tool, but they are obligated to comply with local laws and utilize it responsibly. The developers will not assume any responsibility for potential misuse by users.
--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from base64 import b64encode
 from concurrent.futures import ThreadPoolExecutor
 from time import time
 
@@ -6,31 +5,15 @@ import cv2
 import gradio as gr
 import numpy as np
 import torch
-from cv2 import imencode
-from diffusers import AutoencoderTiny, LCMScheduler
+from diffusers import LCMScheduler
 from diffusers.models import ControlNetModel
 from insightface.app import FaceAnalysis
-from PIL import Image
 
 from download_models import download_instant_id_sdxl_models
 from pipeline_stable_diffusion_xl_instantid import (
     StableDiffusionXLInstantIDPipeline,
     draw_kps,
 )
-
-
-# https://github.com/gradio-app/gradio/issues/2635#issuecomment-1423531319
-def encode_pil_to_base64_new(pil_image):
-    print("-> encode_pil_to_base64_new")
-    image_arr = np.asarray(pil_image)[:, :, ::-1]
-    _, byte_data = imencode(".png", image_arr)
-    base64_data = b64encode(byte_data)
-    base64_string_opencv = base64_data.decode("utf-8")
-    return "data:image/png;base64," + base64_string_opencv
-
-
-# monkey patching encode pil
-gr.processing_utils.encode_pil_to_base64 = encode_pil_to_base64_new
 
 face_adapter = f"./models/sdxl/ip-adapter.bin"
 controlnet_path = f"./models/sdxl/ControlNetModel"
@@ -47,6 +30,7 @@ app = FaceAnalysis(
 )
 app.prepare(ctx_id=0, det_size=(320, 320))
 torch_dtype = torch.float32
+
 controlnet = ControlNetModel.from_pretrained(
     controlnet_path,
     torch_dtype=torch_dtype,
@@ -61,11 +45,6 @@ pipe.to(device)
 pipe.unet = pipe.unet.to(memory_format=torch.channels_last)
 pipe.controlnet = pipe.controlnet.to(memory_format=torch.channels_last)
 
-# pipe.vae = pipe.vae.to(memory_format=torch.channels_last)
-pipe.vae = AutoencoderTiny.from_pretrained(
-    "madebyollin/taesdxl",
-    torch_dtype=torch_dtype,
-)
 pipe.text_encoder = pipe.text_encoder.to(memory_format=torch.channels_last)
 pipe.scheduler = LCMScheduler.from_config(
     pipe.scheduler.config,
@@ -73,6 +52,9 @@ pipe.scheduler = LCMScheduler.from_config(
     beta_end=0.012,
 )
 
+# pipe.scheduler = DEISMultistepScheduler.from_config(
+#     pipe.scheduler.config,
+# )
 pipe.enable_freeu(
     s1=0.6,
     s2=0.4,
@@ -85,8 +67,13 @@ print(pipe)
 def generate_image(
     face_image,
     prompt,
+    identitynet_strength_ratio,
+    adapter_strength_ratio,
 ):
-    start = time()
+    print(f"identitynet_strength_ratio :{identitynet_strength_ratio}")
+    print(f"adapter_strength_ratio :{adapter_strength_ratio}")
+    if prompt == "":
+        prompt = "Photo of a person,high quality"
     face_image = face_image.resize((960, 1024))
     face_info = app.get(cv2.cvtColor(np.array(face_image), cv2.COLOR_RGB2BGR))
     face_info = sorted(
@@ -97,17 +84,15 @@ def generate_image(
     ]  # only use the maximum face
     face_emb = face_info["embedding"]
     face_kps = draw_kps(face_image, face_info["kps"])
-    face_kps.save("kps.jpg")
-    print(time() - start)
 
     # generate image
-    pipe.set_ip_adapter_scale(0.8)
+    pipe.set_ip_adapter_scale(adapter_strength_ratio)
     print("start")
     images = pipe(
         prompt,
         image_embeds=face_emb,
         image=face_kps,
-        controlnet_conditioning_scale=0.8,
+        controlnet_conditioning_scale=identitynet_strength_ratio,
         num_inference_steps=1,
         guidance_scale=0.0,
     ).images
@@ -117,6 +102,8 @@ def generate_image(
 def process_image(
     face_image,
     prompt,
+    identitynet_strength_ratio,
+    adapter_strength_ratio,
 ):
     tick = time()
     with ThreadPoolExecutor(max_workers=1) as executor:
@@ -124,6 +111,8 @@ def process_image(
             generate_image,
             face_image,
             prompt,
+            identitynet_strength_ratio,
+            adapter_strength_ratio,
         )
         images = future.result()
     elapsed = time() - tick
@@ -132,23 +121,31 @@ def process_image(
 
 
 def _get_footer_message() -> str:
-    version = f"<center><p> {APP_VERSION} "
-    footer_msg = version + (
-        '  © 2024 <a href="https://github.com/rupeshs">'
-        " Rupesh Sreeraman</a></p></center>"
-    )
+    version = f"<center><p> v{APP_VERSION}"
+    footer_msg = version
     return footer_msg
+
+
+css = """
+#generate_button {
+    color: white;
+    border-color: #007bff;
+    background: #2563eb;
+
+}
+"""
 
 
 def get_web_ui() -> gr.Blocks:
     with gr.Blocks(
+        css=css,
         title="InstantID CPU",
     ) as web_ui:
         gr.HTML("<center><H1>InstantID CPU</H1></center>")
         with gr.Row():
             with gr.Column():
                 input_image = gr.Image(
-                    label="Init image",
+                    label="Face image",
                     type="pil",
                     height=512,
                 )
@@ -156,7 +153,7 @@ def get_web_ui() -> gr.Blocks:
                     prompt = gr.Textbox(
                         show_label=False,
                         lines=3,
-                        placeholder="A photo of a person",
+                        placeholder="Oil painting",
                         container=False,
                     )
 
@@ -165,14 +162,32 @@ def get_web_ui() -> gr.Blocks:
                         elem_id="generate_button",
                         scale=0,
                     )
+                identitynet_strength_ratio = gr.Slider(
+                    label="IdentityNet strength (for fidelity)",
+                    minimum=0,
+                    maximum=1.5,
+                    step=0.05,
+                    value=0.80,
+                )
+                adapter_strength_ratio = gr.Slider(
+                    label="Image adapter strength (for detail)",
+                    minimum=0,
+                    maximum=1.5,
+                    step=0.05,
+                    value=0.80,
+                )
 
                 input_params = [
                     input_image,
                     prompt,
+                    identitynet_strength_ratio,
+                    adapter_strength_ratio,
                 ]
 
             with gr.Column():
-                gallery = gr.Image(label="Generated Images")
+                gallery = gr.Image(
+                    label="Generated Image",
+                )
             generate_btn.click(
                 fn=process_image,
                 inputs=input_params,
